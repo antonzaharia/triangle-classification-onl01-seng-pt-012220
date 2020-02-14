17ae6e7db942ed5acd3b6dd050556348538889af
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-14 14:38:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,14 +12,15 @@ class Triangle
     
   if @side_1 > 0 && @side_2 > 0 && @side_3 > 0 
       raise TriangleError
+  elsif @side_1 + @side_2 + @side_3 
   else
-    if @side_1 == @side_2 && @side_2 == @side_3
-      :equilateral
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
-      :isosceles
-    else
-      :scalene
-    end
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+        :isosceles
+      else
+        :scalene
+      end
   end
     
   end
