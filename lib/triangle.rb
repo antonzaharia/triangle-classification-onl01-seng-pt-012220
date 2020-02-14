2b061534ecf9e2b33ec89c79e4a0af513a0e2598
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-14 14:23:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,16 @@ class Triangle
   
   attr_accessor :equilateral, :isosceles, :scalene
   
-  def initialize()
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind 
+    
+    if @side_1 == @side_2 == @side_3
+      
     
   end
   
