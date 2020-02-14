602b06f6fa65be43f977187d7bdd19aaa93ab3dc
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-14 14:27:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,8 @@ class Triangle
     
     if @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+      
     end
     
   end
